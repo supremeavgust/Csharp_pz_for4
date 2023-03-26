0cca4e409e8a4807ce54960090466c414d708139
Stanislav <avgustwot@mail.ru>
2023-03-26 15:01:25 +0300

    Задача 26

---
+++ example26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int f(int n)
+{ if(n==0)
+    return 1;
+    int count=0;
+    while(n>0)
+    {
+        n/=10;
+        count ++;
+    }
+    return count;
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
