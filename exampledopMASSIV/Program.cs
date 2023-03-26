62c40c588b27909372dbe8f3f982764a67dcc524
Stanislav <avgustwot@mail.ru>
2023-03-26 15:23:52 +0300

    Задача дополнительная 2 массив

---
+++ exampledopMASSIV/Program.cs
@@ -0,0 +1,12 @@
+﻿// второй максиум
+Console.Clear();
+
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// "5 4 3 2"
+// "5", "4", "3", "2"
+// 5, 4, 3, 2
+// [5, 4, 3, 2]
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+foreach (int elem in array)
+    Console.WriteLine(elem * 100);
