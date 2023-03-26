091372637a1a86224b1f08ff30228dffa06cf000
Stanislav <avgustwot@mail.ru>
2023-03-26 15:04:30 +0300

    Задача 37

---
+++ example37/Program.cs
@@ -0,0 +1,35 @@
+﻿// // Задача 37: Найдите произведение пар чисел в 
+// одномерном массиве. 
+// Парой считаем первый и последний элемент, второй
+//  и предпоследний и т.д. Результат запишите
+//   в новом массиве.
+// // [1 2 3 4 5] -> 5 8 3
+// // [6 7 3 6] -> 36 21
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10); // [0;9]
+}
+
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2+ array.Length%2; i++)
+        Console.Write($"{array[i] * array[array.Length - 1 - i]} ");
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+Console.Clear();
+Console.Write($"n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+PrintArray(array);
+ReleaseArray(array);
