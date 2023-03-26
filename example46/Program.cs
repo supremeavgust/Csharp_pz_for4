f32c9d1e4df264e7f0922c62257c3f0f66987665
Stanislav <avgustwot@mail.ru>
2023-03-26 15:06:43 +0300

    Задача 46

---
+++ example46/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+//  заполненный случайными целыми числами.
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(-10, 10);
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+  }
+
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
