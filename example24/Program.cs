669c4a76af6b4f10b30d59f0ba4de32d2fca3601
Stanislav <avgustwot@mail.ru>
2023-03-26 15:01:07 +0300

    Задача 24

---
+++ example24/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+double f(double n)
+{
+    return((1+n)/2*n);
+}
+
+Console.Clear();
+double a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a));
