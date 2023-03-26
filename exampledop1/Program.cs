45979f3ac56de9070a56354c7e22ada388470cf0
Stanislav <avgustwot@mail.ru>
2023-03-26 15:23:24 +0300

    Задача дополнительное 1

---
+++ exampledop1/Program.cs
@@ -0,0 +1,20 @@
+﻿// второй максиум
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int maxFirst = n, maxSecond = -1;
+
+while(n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+
+    if (n > maxFirst)
+    {
+        maxSecond = maxFirst;
+        maxFirst = n;
+    }
+    else if (n > maxSecond)
+        maxSecond = n;
+}
+
+Console.WriteLine(maxSecond);
