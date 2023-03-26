6d9af255975c20920f0350d1f2c3365ef50dab0d
Stanislav <avgustwot@mail.ru>
2023-03-26 15:00:52 +0300

    Задача 42

---
+++ example 42/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string res = String.Empty;
+while(n > 0)
+{
+    res = Convert.ToString(n % 2) + res;
+    n /= 2;
+}
+Console.WriteLine(res);
