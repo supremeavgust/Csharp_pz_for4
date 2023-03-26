b95e42b1ace62c006a7c4bf81dbef820d250e6a9
Stanislav <avgustwot@mail.ru>
2023-03-26 15:04:08 +0300

    Задача 33

---
+++ example33/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, -3] -> да
+
+// task 33
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 10); // [0;9]
+        
+}
+
+
+string ReleaseArray(int[] array, int k)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == k)
+            return "yes";
+    return "no";
+        
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+Console.Clear();
+Console.Write($"n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"k = ");
+int k = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+PrintArray(array);
+Console.WriteLine(ReleaseArray(array, k));
