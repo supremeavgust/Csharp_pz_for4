3c4080846ad4426d0e42a67e794007cba64eaf43
Stanislav <avgustwot@mail.ru>
2023-03-26 15:30:53 +0300

    Второй вариант 51 задачи

--- example51/Program.cs
+++ example51/Program.cs
@@ -1,44 +1,52 @@
-﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, \
-// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Сумма элементов главной диагонали: 1+9+2 = 12
+﻿// Задача 51. Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1,1) и т.д.))
 
-    
-
-void InputMatrix(int[,] matrix)
+int numb(string message)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    Console.WriteLine(message);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+void Random(int[,]arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < arg.GetLength(1); j++)
         {
-            matrix[i,j] = new Random().Next(1, 10);
-            Console.Write($"{matrix[i,j]} \t");
+            arg[i,j] = new Random().Next(1,100);
         }
-        Console.WriteLine();
     }
 }
 
-
-int ReleaseMatrix(int[,] matrix)
+void PrintArray(int[,] arg)
 {
-    int summa = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < arg.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < arg.GetLength(1); j++)
         {
-            if (i == j)
-                summa += matrix[i,j];
+        Console.Write($"{arg[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int Summa(int [,]arg)
+{
+    int result = 0;
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+        if(i==j) result += arg[i,j];
         }
     }
-    return summa;
+    return result;
 }
-Console.Clear();
-Console.Write("Введите размер матрицы: ");
-int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int[,] matrix = new int[size[0], size[1]];
-InputMatrix(matrix);
-Console.WriteLine();
-Console.WriteLine(ReleaseMatrix(matrix));
+
+int rows = numb($"Введите длину строк: ");
+int columns = numb ($"Введите длину столбцов: ");
+int[,]Array = new int [rows, columns];
+Random(Array);
+PrintArray(Array);
+Console.Write($"Сумма элементов на главной диагонали равна {Summa(Array)}");
